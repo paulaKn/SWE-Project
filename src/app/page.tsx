411eb2f3c4ac968a62618e1314e27a38c68e6503
Paula Knöller <paula.knoeller@gmail.com>
2024-12-31 18:37:37 +0100

    add page.tsx für login Aufruf

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Login from "@/components/Login";
+
+export default function HomePage() {
+ 
+
+
+  
+  return (
+    <div>
+      <h1></h1>
+      <Login></Login>
+    </div>
+  );
+}
